80bc0c501240c5092f024e872b4faddecd8176a9
Mercedes <shatunovmaksim3@gmail.com>
2026-04-25 20:56:18 +0300

    Add registration button to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,5 +41,13 @@
                 Войти
             </x-primary-button>
         </div>
+
+        @if (Route::has('register'))
+            <div class="mt-4 text-center">
+                <a href="{{ route('register') }}" class="inline-block w-full text-center px-4 py-2 border border-green-600 rounded-md text-sm font-medium text-green-700 hover:bg-green-50 transition">
+                    Зарегистрироваться
+                </a>
+            </div>
+        @endif
     </form>
 </x-guest-layout>
